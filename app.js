66908c9a51e7063d4a3f890891dfa37e21c97567
habibyeo2 <habibyeo2@gmail.com>
2020-07-27 22:43:08 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,95 @@
+console.log(a)
+
+
+console.clear()
+console.log(output)
+
+
+console.log(typeof seven);
+console.log(typeof three);
+
+
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
+
+
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
+
+
+let innerHtml = "<p>Click here to <a href=\"#Home\">return home</a></p>";
+console.log(innerHtml);
+
+
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if (x == y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
+
+
+
+function getNine() {
+    let x = 6;
+    let y = 3;
+    return x + y;
+  }
+  
+  let result = getNine();
+  console.log(result);
+
+
+
+
+function raiseToPower(b, e) {
+    return Math.pow(b, e);
+  }
+  
+  let base = 2;
+  let exp = 3;
+  let power = raiseToPower(base, exp);
+  console.log(power);
+
+
+  function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+    // Fix the line below
+    for (let i = 0; i < len; i++) {
+      // Do not alter code below this line
+      console.log(firstFive[i]);
+    }
+  }
+
+
+
+  function zeroArray(m, n) {
+    let newArray = [];
+    for (let i = 0; i < m; i++) {
+      let row = [];
+      for (let j = 0; j < n; j++) {
+        row.push(0);
+      }
+      newArray.push(row);
+    }
+    return newArray;
+  }
+  let matrix = zeroArray(3, 2);
+  console.log(matrix);
+
+
+
+  function myFunc() {
+    for (let i = 1; i <= 4; i += 2) {
+      console.log("Still going!");
+    }
+  }
---
+++ app2.js
@@ -0,0 +1,21 @@
+let yourArray = ["a", 2, true, "c", null,];
+
+
+arr.unshift("I", 2, "three");
+  arr.push(7, "VIII", 9);
+  let popped = arr.pop(); 
+    let shifted = arr.shift(); 
+
+
+
+
+
+arr.splice(1,4)
+
+
+
+arr.splice(0,2, 'DarkSalmon', 'BlanchedAlmond')
+
+
+
+return arr.slice(2, 4);
